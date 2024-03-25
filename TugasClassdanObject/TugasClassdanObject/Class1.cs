ad52072f212193321090a89e91e794c1b16f64f8
junior <junior.yesaya.awawangi@gmail.com>
2024-03-25 10:22:06 +0700

    tugas3

---
+++ TugasClassdanObject/TugasClassdanObject/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassdanObject
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDrive {  get; set; }
+        public int NumPassenger { get; set; }
+
+    
+
+    public void TaxiInfo()
+    {
+        Console.WriteLine("Nama Driver : {0}", DriverName);
+        Console.WriteLine("On Duty : {0}", OnDrive);
+        Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+        Console.WriteLine();
+    }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput", DriverName);
+            Console.WriteLine();
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} penumpang telah diturunkan", DriverName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TugasClassdanObject/TugasClassdanObject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassdanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat object Taxi
+            Taxi taxi = new Taxi();
+
+            //Pengesetan nilau properti
+            taxi.DriverName = "Jono";
+            taxi.OnDrive = true;
+            taxi.NumPassenger = 10;
+
+            //Pemanggilan method
+            taxi.TaxiInfo();  
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+        }
+    }
+}
